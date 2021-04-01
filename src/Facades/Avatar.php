50c2339354a1b564bfb33995b62b970ac16cc32c
wanglele <357620290@qq.com>
2021-04-01 20:04:58 +0800

    test

---
+++ src/Facades/Avatar.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Cxp\Avatar\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Avatar extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'avatar';
+    }
+}
