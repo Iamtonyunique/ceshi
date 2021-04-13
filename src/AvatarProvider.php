a150e6d4933ef138905795cfa1f8b3fd43a1fff0
Iamtonyunique <39897438+Iamtonyunique@users.noreply.github.com>
2021-04-13 13:44:57 +0800

    Update AvatarProvider.php

--- src/AvatarProvider.php
+++ src/AvatarProvider.php
@@ -2,31 +2,10 @@
 
 
 namespace Cxp\Avatar;
-use Illuminate\Support\ServiceProvider;
 
-class AvatarProvider extends ServiceProvider
+class AvatarProvider 
 {
-    /**
-     * Bootstrap the application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        // 发布配置文件
-        $this->publishes([
-            __DIR__.'/config/avatar.php' => config_path('avatar.php'),
-        ]);
-    }
-    /**
-     * Register the application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        $this->app->singleton('avatar', function ($app) {
-            return new Avatar($app['config']);
-        });
-    }
+   public function test(){
+        echo 1111111;
+   }
 }
